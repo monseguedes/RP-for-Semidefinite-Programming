adbb253cb37bf944b346363daf186854f317eaf9
Monse Guedes Ayala <s1553571@ed.ac.uk>
2024-01-21 11:29:27 +0900

    Create column_generation.py

---
+++ column_generation.py
@@ -0,0 +1,7 @@
+"""
+This module contains the implementation of the column generation algorithm
+for polynomial optimization, taken from the paper
+- "Optimization Over Structured Subsets of Positive Semidefinite Matrices
+via Column Generation" by Amir Ali Ahmadi, Sanjeeb Dash, and Georgina Hall.
+
+"""

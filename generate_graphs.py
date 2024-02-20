f0cc0dfcacc5f87fc13f756dc3adac76880ee957
Monse Guedes Ayala <s1553571@ed.ac.uk>
2024-02-20 06:50:30 +0000

    Update generate_graphs.py

--- generate_graphs.py
+++ generate_graphs.py
@@ -28,7 +28,7 @@ class Graph:
         Get the complement of the graph.
 
         """
-            
+
         complement = []
 
         for i in range(self.n):
@@ -37,13 +37,17 @@ class Graph:
                     complement.append((i, j))
 
         self.edges = complement
-         
+
     def get_picking_SOS(self, verbose=False):
         """ """
 
-        monomial_matrix = monomials.stable_set_monomial_matrix(self.edges, self.n, level=1)
+        monomial_matrix = monomials.stable_set_monomial_matrix(
+            self.edges, self.n, level=1
+        )
 
-        distinct_monomials = monomials.stable_set_distinct_monomials(self.edges, self.n, level=1)
+        distinct_monomials = monomials.stable_set_distinct_monomials(
+            self.edges, self.n, level=1
+        )
         self.distinct_monomials_L1 = distinct_monomials
 
         if verbose:
@@ -80,12 +84,16 @@ class Graph:
         """
         if verbose:
             print("Building monomial matrix for level 2")
-        monomial_matrix = monomials.stable_set_monomial_matrix(self.edges, self.n, level=2) 
+        monomial_matrix = monomials.stable_set_monomial_matrix(
+            self.edges, self.n, level=2
+        )
         if verbose:
-            print('SIZE OF MONOMIAL MATRIX:', len(monomial_matrix))
+            print("SIZE OF MONOMIAL MATRIX:", len(monomial_matrix))
             print("Done building monomial matrix for level 2")
             print("Building distinct monomials for level 2")
-        self.distinct_monomials_L2 = monomials.stable_set_distinct_monomials(self.edges, self.n, level=2)
+        self.distinct_monomials_L2 = monomials.stable_set_distinct_monomials(
+            self.edges, self.n, level=2
+        )
         if verbose:
             print("Done building distinct monomials for level 2")
 
@@ -99,12 +107,17 @@ class Graph:
         self.A_L2 = {}
         for i, monomial in enumerate(self.distinct_monomials_L2):
             if verbose:
-                print("Picking monomial: {} out of {}".format(i, len(self.distinct_monomials_L2)))
-            self.A_L2[monomial] = monomials.pick_specific_monomial(monomial_matrix, monomial)
-           
+                print(
+                    "Picking monomial: {} out of {}".format(
+                        i, len(self.distinct_monomials_L2)
+                    )
+                )
+            self.A_L2[monomial] = monomials.pick_specific_monomial(
+                monomial_matrix, monomial
+            )
+
         if verbose:
             print("Done building Ai matrices for level 2")
-
 
     def store_graph(self, name):
         """
@@ -157,6 +170,19 @@ class Graph:
                 if self.graph[i][j] == 1:
                     edges.append((i, j))
         self.edges = edges
+
+    def get_matrix_from_edges(self):
+        """
+        Get the adjacency matrix from the edges of the graph.
+
+        """
+
+        graph = np.array([[0 for i in range(self.n)] for j in range(self.n)])
+        for i, j in self.edges:
+            graph[i][j] = 1
+            graph[j][i] = 1
+        self.graph = graph
+
 
 def generate_pentagon():
     """
@@ -235,7 +261,23 @@ def generate_petersen_graph():
     """
     petersen = Graph()
     petersen.n = 10
-    petersen.edges = [(0, 1), (1, 2), (2, 3), (3, 4), (4, 0), (5, 6), (6, 7), (7, 8), (8, 9), (9, 5), (0, 5), (1, 6), (2, 7), (3, 8), (4, 9)]
+    petersen.edges = [
+        (0, 1),
+        (1, 2),
+        (2, 3),
+        (3, 4),
+        (4, 0),
+        (5, 6),
+        (6, 7),
+        (7, 8),
+        (8, 9),
+        (9, 5),
+        (0, 5),
+        (1, 6),
+        (2, 7),
+        (3, 8),
+        (4, 9),
+    ]
     # Get matrix representation of the graph
     petersen.graph = np.array(
         [[0 for i in range(petersen.n)] for j in range(petersen.n)]
@@ -251,44 +293,116 @@ def generate_petersen_graph():
     petersen.store_graph("petersen")
 
 
-def generate_cordones(n):
+def generate_cordones(n, complement=False):
+    """ 
+    Generate a cordones graph.
+
+    This graph looks like:
+
+    [0, 1, 1, ..., 1, 0, ..., 0, 0, ..., 0]
+    [1, 0, 0, ..., 0, 1, ..., 1  0, ..., 0]
+    [1, 0, 0, ..., 0, 0, ..., 1, 1, ..., 0]
+    [., ., ., ..., ., ., ..., ., ., ..., .]
+    [1, 0, 0, ..., 0, 1, ..., 0, 0, ..., 1]
+    [0, 1, 0, ..., 1, 0, ..., 0, 1, ..., 0]
+    [., ., ., ..., ., ., ..., ., ., ..., 0]
+    [0, 1, 1, ..., 0, 0, ..., 0, 0, ..., 1]
+    [0, 0, 1, ..., 0, 1, ..., 0, 0, ..., 0] 
+    [., ., ., ..., ., ., ..., ., ., ..., 0]
+    [0, 0, 0, ..., 1, 0, ..., 1, 0, ..., 0]
     """
-    
-    """
-    
+
     Jn = np.ones((n, n))
     In = np.eye(n)
     en = np.ones((n, 1))
     zn = np.zeros((n, 1))
     Zn = np.zeros((n, n), dtype=int)
 
-    A = np.block([[0,  1,  np.transpose(en),   np.transpose(zn),   np.transpose(zn)], 
-                  [1,  0,  np.transpose(zn),   np.transpose(en),   np.transpose(zn)],
-                  [en, zn, Zn,                 Jn-In,              In],
-                  [zn, en, Jn-In,              Zn,                 In], 
-                  [zn, zn, In,                 In,                 Zn]])
-    
+    A = np.block(
+        [
+            [0, 1, np.transpose(en), np.transpose(zn), np.transpose(zn)],
+            [1, 0, np.transpose(zn), np.transpose(en), np.transpose(zn)],
+            [en, zn, Zn, Jn - In, In],
+            [zn, en, Jn - In, Zn, In],
+            [zn, zn, In, In, Zn],
+        ]
+    )
+
     cordones = Graph()
     cordones.n = A.shape[0]
     cordones.graph = A
     cordones.get_edges_from_matrix()
+    if complement:
+        cordones.edges_complement_graph()
+        cordones.get_matrix_from_edges()
     cordones.plot_graph()
     cordones.get_picking_SOS(verbose=True)
     cordones.picking_for_level_two(verbose=True)
-    cordones.store_graph("cordones_{}".format(n))
+    if complement:
+        cordones.store_graph("cordones_{}_complement".format(n))
+    else:
+        cordones.store_graph("cordones_{}".format(n))
+
+
+def more_connected_cordones(n, complement=False):
+
+    """ 
+    Generate a cordones graph.
+
+    This graph looks like:
+
+    """
+
+    Jn = np.ones((n, n))
+    In = np.eye(n)
+    en = np.ones((n, 1))
+    zn = np.zeros((n, 1))
+    Zn = np.zeros((n, n), dtype=int)
+
+    A = np.block(
+        [
+            [0, 1, np.transpose(en), np.transpose(zn), np.transpose(zn)],
+            [1, 0, np.transpose(zn), np.transpose(en), np.transpose(zn)],
+            [en, zn, Zn, Jn - In, In],
+            [zn, en, Jn - In, Zn, In],
+            [zn, zn, In, In, Zn],
+        ]
+    )
+
+    cordones = Graph()
+    cordones.n = A.shape[0]
+    cordones.graph = A
+    cordones.get_edges_from_matrix()
+    if complement:
+        cordones.edges_complement
+        cordones.get_matrix_from_edges()
+    cordones.plot_graph()
+    cordones.get_picking_SOS(verbose=True)
+    cordones.picking_for_level_two(verbose=True)
+    if complement:
+        cordones.store_graph("new_cordones_{}_complement".format(n))
+    else:
+        cordones.store_graph("new_cordones_{}".format(n))
 
 
 def generate_6_wheel_graph():
-    """
-    
-    """
+    """ """
     wheel = Graph()
     wheel.n = 6
-    wheel.edges = [(0, 1), (1, 2), (2, 3), (3, 4), (4,0), (0, 5), (1,5), (2,5), (3,5), (4,5)]
+    wheel.edges = [
+        (0, 1),
+        (1, 2),
+        (2, 3),
+        (3, 4),
+        (4, 0),
+        (0, 5),
+        (1, 5),
+        (2, 5),
+        (3, 5),
+        (4, 5),
+    ]
     # Get matrix representation of the graph
-    wheel.graph = np.array(
-        [[0 for i in range(wheel.n)] for j in range(wheel.n)]
-    )
+    wheel.graph = np.array([[0 for i in range(wheel.n)] for j in range(wheel.n)])
     for i, j in wheel.edges:
         wheel.graph[i][j] = 1
         wheel.graph[j][i] = 1
@@ -305,10 +419,10 @@ def generate_generalised_petersen(n, k, complement=False):
     Generate a generalised petersen graph.
     """
     petersen = Graph()
-    petersen.n = 2*n
-    inner_star_edges = [(i, (i+k) % n) for i in range(n)]
-    outer_star_edges = [(i, i+1) for i in range(n, 2*n - 1)] + [(2*n - 1, n)]
-    connecting_edges = [element for element in zip(range(n + 1),range(n, 2*n ))]
+    petersen.n = 2 * n
+    inner_star_edges = [(i, (i + k) % n) for i in range(n)]
+    outer_star_edges = [(i, i + 1) for i in range(n, 2 * n - 1)] + [(2 * n - 1, n)]
+    connecting_edges = [element for element in zip(range(n + 1), range(n, 2 * n))]
     petersen.edges = inner_star_edges + outer_star_edges + connecting_edges
     if complement:
         petersen.edges_complement_graph()
@@ -321,32 +435,14 @@ def generate_generalised_petersen(n, k, complement=False):
         petersen.graph[i][j] = 1
         petersen.graph[j][i] = 1
 
+    print("Pickings for level 1:")
+    petersen.plot_graph()
     petersen.get_picking_SOS(verbose=True)
     petersen.picking_for_level_two(verbose=True)
     if complement:
         petersen.store_graph("generalised_petersen_{}_{}_complement".format(n, k))
     else:
         petersen.store_graph("generalised_petersen_{}_{}".format(n, k))
-
-
-def generate_hexagon():
-    """
-    Generate a hexagon graph.
-    """
-    hexagon = Graph()
-    hexagon.n = 6
-    hexagon.edges = [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 0)]
-    # Get matrix representation of the graph
-    hexagon.graph = np.array(
-        [[0 for i in range(hexagon.n)] for j in range(hexagon.n)]
-    )
-    for i, j in hexagon.edges:
-        hexagon.graph[i][j] = 1
-        hexagon.graph[j][i] = 1
-    hexagon.get_picking_SOS()
-    hexagon.get_picking_edges()
-    hexagon.picking_for_level_two()
-    hexagon.store_graph("hexagon")
 
 
 def generate_probability_graph(n, p, seed=0):
@@ -374,7 +470,7 @@ def generate_probability_graph(n, p, seed=0):
     prob.n = n
     prob.graph = np.random.choice([0, 1], size=(n, n), p=[1 - p, p])
     prob.graph = np.triu(prob.graph, k=1) + np.triu(prob.graph, k=1).T
-    
+
     prob.get_edges_from_matrix()
     prob.plot_graph()
     prob.get_picking_SOS(verbose=True)
@@ -404,37 +500,46 @@ def generate_connected_imperfect_graph(repetitions, p, type="6_wheel"):
     np.random.seed(0)
 
     if type == "6_wheel":
-        initial_edges = [(0, 1), (1, 2), (2, 3), (3, 4), (4,0), (0, 5), (1,5), (2,5), (3,5), (4,5)]
+        initial_edges = [
+            (0, 1),
+            (1, 2),
+            (2, 3),
+            (3, 4),
+            (4, 0),
+            (0, 5),
+            (1, 5),
+            (2, 5),
+            (3, 5),
+            (4, 5),
+        ]
         edges = []
         for i in range(1, repetitions):
-            new_edges = [(x + 6*i, y + 6*i) for x, y in initial_edges]
+            new_edges = [(x + 6 * i, y + 6 * i) for x, y in initial_edges]
             edges += new_edges
-        edges = initial_edges + edges   
+        edges = initial_edges + edges
 
     # Loop over all vertices and connect them with probability p to the vertices of other graphs
     for i in range(repetitions):
-        for vertex in range(i*6, (i+1)*6):
+        for vertex in range(i * 6, (i + 1) * 6):
             print("Vertex:", vertex)
             for j in [graph for graph in range(repetitions) if graph not in [i]]:
                 print("Graph:", j)
-                for vertex2 in range(j*6, (j+1)*6):
+                for vertex2 in range(j * 6, (j + 1) * 6):
                     print("Vertex2:", vertex2)
                     if np.random.rand() < p:
                         edges.append((vertex, vertex2))
-    
+
     graph = Graph()
-    graph.n = 6*repetitions 
+    graph.n = 6 * repetitions
     graph.edges = edges
-    
+
     # Get matrix representation of the graph
-    graph.graph = np.array(
-        [[0 for i in range(graph.n)] for j in range(graph.n)]
-    )
+    graph.graph = np.array([[0 for i in range(graph.n)] for j in range(graph.n)])
     for i, j in graph.edges:
         graph.graph[i][j] = 1
         graph.graph[j][i] = 1
 
-    graph.plot_graph()  
+    graph.plot_graph()
     graph.get_picking_SOS()
     graph.picking_for_level_two(verbose=True)
     graph.store_graph("connected_{}_{}_{}".format(type, repetitions, p))
@@ -447,7 +552,7 @@ if __name__ == "__main__":
     # generate_petersen_graph()
     # generate_6_wheel_graph()
     generate_generalised_petersen(30, 2, complement=True)
-    # generate_cordones(7)
+    # generate_cordones(5, complement=False)
+    # more_connected_cordones(5, complement=False)
     # generate_probability_graph(30, 0.6, seed=0)
     # generate_connected_imperfect_graph(5, 0.4)
-
